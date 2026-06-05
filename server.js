2cea49a805299ebebd5f2746ef350411e3fbf354
yanivedery29 <yaniv.edery29@gmail.com>
2026-06-05 13:46:51 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Raz Auto Reply Server is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
